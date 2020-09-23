e7e22fe04ae7abe879175e5aaf34cd66d31718e5
GVabal <gvabal@gmail.com>
2020-09-23 22:45:12 +0300

    Added ItemServiceTest

--- src/test/java/dev/vabalas/warehouseservice/service/ItemServiceTest.java
+++ src/test/java/dev/vabalas/warehouseservice/service/ItemServiceTest.java
@@ -1,16 +1,54 @@
 package dev.vabalas.warehouseservice.service;
 
+import dev.vabalas.warehouseservice.entity.Item;
+import dev.vabalas.warehouseservice.repository.ItemRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+import java.util.List;
 
+@ExtendWith(MockitoExtension.class)
 class ItemServiceTest {
 
-    @Test
-    void getItemsWithQuantityLessThan() {
+    List<Item> items = List.of(
+            new Item("Item 1", 1, LocalDate.parse("2019-12-31")),
+            new Item("Item 2", 5, LocalDate.parse("2020-12-31")),
+            new Item("Item 3", 10, LocalDate.parse("2021-12-31"))
+    );
+    Page<Item> itemsMock = new PageImpl<>(items, Pageable.unpaged(), items.size());
+
+    @Mock
+    private ItemRepository itemRepositoryMock;
+
+    @InjectMocks
+    private ItemService itemService;
+
+    @BeforeEach
+    void setUp() {
+        Mockito.when(itemRepositoryMock.findAll(Pageable.unpaged())).thenReturn(itemsMock);
     }
 
     @Test
-    void getItemsWithExpirationDateBefore() {
+    void returns_correct_number_of_items_with_quantity_less_than() {
+        Assertions.assertEquals(
+                itemService.getItemsWithQuantityLessThan(7, Pageable.unpaged()).getSize(),
+                2);
+    }
+
+    @Test
+    void returns_correct_number_of_items_with_expiration_date_before() {
+        Assertions.assertEquals(
+                itemService.getItemsWithExpirationDateBefore(LocalDate.parse("2021-04-01"), Pageable.unpaged()).getSize(),
+                2);
     }
 }
