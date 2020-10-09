07130c46dafd86d23727caa9e5d29cffce07bf69
GVabal <gvabal@gmail.com>
2020-10-09 15:44:46 +0300

    Removed unused imports

--- src/main/java/dev/vabalas/warehouseservice/service/ItemService.java
+++ src/main/java/dev/vabalas/warehouseservice/service/ItemService.java
@@ -7,13 +7,10 @@ import dev.vabalas.warehouseservice.repository.ItemRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class ItemService {
